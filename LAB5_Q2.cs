c0f6cd84aa15e3ad1782fdc855a372155153e3c4
MUSA <musamunir741@gmail.com>
2024-10-22 22:45:47 +0500

    Add files via upload

---
+++ LAB5_Q2.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+public class Customer
+{
+    public string CustomerId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    // Constructor
+    public Customer(string Id, string FName, string LName)
+    {
+        CustomerId = Id;
+        FirstName = FName;
+        LastName = LName;
+    }
+}
+
+public class RetailCustomer : Customer
+{
+    public string CreditCardType { get; set; }
+    public string CreditCardNo { get; set; }
+
+    public RetailCustomer(string Id, string FName, string LName, string CType, string CNo) : base(Id, FName, LName)
+    {
+        CreditCardType = CType;
+        CreditCardNo = CNo;
+    }
+}
+
+public class CorporateCustomer : Customer
+{
+    public string CompanyName { get; set; }
+    public int FrequentFlyerPoints { get; set; }
+
+    public CorporateCustomer(string Id, string FName, string LName, string companyName, int points) : base(Id, FName, LName)
+    {
+        CompanyName = companyName;
+        FrequentFlyerPoints = points;
+    }
+}
+
+public class Reservation
+{
+    public string ReservationNo { get; set; }
+    public DateTime Date { get; set; }
+
+    public Reservation(string No, DateTime date)
+    {
+        ReservationNo = No;
+        Date = date;
+    }
+}
+
+public class Seat
+{
+    public int RowNo { get; set; }
+    public int SeatNo { get; set; }
+    public decimal Price { get; set; }
+
+    public Seat(int rowNo, int seatNo, decimal price)
+    {
+        RowNo = rowNo;
+        SeatNo = seatNo;
+        Price = price;
+    }
+}
+
+public class Flight
+{
+    public string FlightId { get; set; }
+    public DateTime Date { get; set; }
+    public string Origin { get; set; }
+    public string Destination { get; set; }
+
+    public Flight(string flightId, DateTime date, string origin, string destination)
+    {
+        FlightId = flightId;
+        Date = date;
+        Origin = origin;
+        Destination = destination;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Creating and displaying a retail customer
+        RetailCustomer retailCustomer = new RetailCustomer("233598", "Musa", "Munir", "Visa", "1234-5678-910-11");
+        Console.WriteLine($"Retail Customer: {retailCustomer.FirstName} {retailCustomer.LastName}, Card: {retailCustomer.CreditCardType} - {retailCustomer.CreditCardNo}");
+
+        // Creating and displaying a corporate customer
+        CorporateCustomer corpCustomer = new CorporateCustomer("233600", "Hanan", "Zia", "Avatar", 10000);
+        Console.WriteLine($"Corporate Customer: {corpCustomer.FirstName} {corpCustomer.LastName}, Company: {corpCustomer.CompanyName}, Points: {corpCustomer.FrequentFlyerPoints}");
+    }
+}
---
+++ LAb5_Q1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+public enum Department
+{
+    ComputerScience,
+    SoftwareEngineering,
+    CyberSecurity
+}
+
+public class Person
+{
+    public string Name { get; set; }
+
+    // No argument constructor
+    public Person()
+    {
+        Name = "Null";
+    }
+
+    // Multi-argument constructor
+    public Person(string name)
+    {
+        Name = name;
+    }
+}
+
+public class Student : Person
+{
+    public string RegNo { get; set; }
+    public int Age { get; set; }
+    public Department Program { get; set; }
+
+    // No argument constructor
+    public Student() : base()
+    {
+        RegNo = "Null";
+        Age = 0;
+        Program = Department.ComputerScience;
+    }
+
+    // Multi-argument constructor
+    public Student(string name, string regNo, int age, Department program) : base(name)
+    {
+        RegNo = regNo;
+        Age = age;
+        Program = program;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Student student1 = new Student();
+        Console.WriteLine($"Student1: Name = {student1.Name}, RegNo = {student1.RegNo}, Age = {student1.Age}, Program = {student1.Program}");
+
+        Student student2 = new Student("Musa", "233598", 20, Department.ComputerScience);
+        Console.WriteLine($"Student2: Name = {student2.Name}, RegNo = {student2.RegNo}, Age = {student2.Age}, Program = {student2.Program}");
+    }
+}
+
+
+
